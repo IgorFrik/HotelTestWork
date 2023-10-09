b6077f3706a0096bf010a617c0320a0dac371dd0
IgorFrik <murlocov@icloud.com>
2023-10-09 15:44:29 +0700

    New Update

--- HotelTestWork/NetWork/NetWork.swift
+++ HotelTestWork/NetWork/NetWork.swift
@@ -48,3 +48,21 @@ extension HotelNetWork: HotelNetWorkProtocol {
         }
     }
 }
+
+class UnsafeTask<T> {
+    let semaphore = DispatchSemaphore(value: 0)
+    private var result: T?
+    init(block: @escaping () async -> T) {
+        Task {
+            result = await block()
+            semaphore.signal()
+        }
+    }
+
+
+    func get() -> T {
+        if let result = result { return result }
+        semaphore.wait()
+        return result!
+    }
+}
